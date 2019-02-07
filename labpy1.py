0f817da3005d5cfa24ccaa75e292ac8d77f97f84
refhaldo <46928851+refhaldo@users.noreply.github.com>
2019-02-08 02:20:41 +0700

    Add files via upload

---
+++ labpy1.py
@@ -0,0 +1,13 @@
+a = int(input('Bilangan 1 :'))
+b = int(input('Bilangan 2 :'))
+c = int(input('Bilangan 3 :'))
+
+print ('Masukkan Bilangan Yang Diinginkan')
+if a>b and a>c:
+    print ('Bilangan Terbesar Adalah :', a)
+elif b>c and b>c:
+    print ('Bilangan Terbesar Adalah :', b)
+else:
+    print ('Bilangan Terbesar Adalah :', c)
+print ('')
+print ('Terimakasih')
